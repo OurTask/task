0603de7bedef0a88751d8750e67c393147bbde6f
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-20 19:58:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='task',
+    name='ourtask_task',
     version='0.1',
     packages=find_packages(),
     description='A simple JSON processing package',
