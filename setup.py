886c978ad142a4f2f0912c1f2979cecdf5ccb72c
Willem van Heemstra <wvanheemstra@icloud.com>
2024-10-21 09:13:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
 from setuptools import setup
 
 setup()
-
-# from setuptools import setup, find_packages
-
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-
-# with open("requirements.txt") as f:
-#     required = f.read().splitlines()
-
-# setup(
-#     name="ourtask_task",
-#     use_scm_version=True,
-#     setup_requires=["setuptools>=42", "setuptools_scm"],
-#     packages=find_packages(include=["task"]),
-#     description="A simple JSON processing package.",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     author="Willem van Heemstra",
-#     author_email="wvanheemstra@icloud.com",
-#     license="MIT",
-#     install_requires=required,
-#     url="https://github.com/OurTask/task",
-#     classifiers=[
-#         "Programming Language :: Python :: 3",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: OS Independent",
-#     ],
-#     python_requires=">=3.6",
-# )
